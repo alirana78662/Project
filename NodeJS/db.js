929847b0a6e491a6cf617aa9b589f864f7db50e0
Ali Rana <alirana78662@gmail.com>
2019-11-03 22:40:01 -0800

    My Api Work

---
+++ NodeJS/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set(' useUnifiedTopology',true);
+
+mongoose.connect('mongodb://localhost:27017/CrudDB', (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded..');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
